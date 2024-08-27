6f5fe317ff1467a75a13e058c85f6192706e737f
Riley Ward <riley4242@hotmail.com>
2024-08-26 21:48:20 -0600

    Create DB schema and populate sample data

--- src/docker-compose/spring-backend/src/main/resources/data.sql
+++ src/docker-compose/spring-backend/src/main/resources/data.sql
@@ -1,1 +1,20 @@
-INSERT INTO GREETINGS(name) values ('Docker');
+INSERT INTO public.player(name)
+VALUES ('Exits');
+
+INSERT INTO public.player(name)
+VALUES ('Possession');
+
+INSERT INTO public.player(name)
+VALUES ('Theory');
+
+INSERT INTO public.player(name)
+VALUES ('Iota');
+
+INSERT INTO public.player(name)
+VALUES ('Sentient');
+
+INSERT INTO public.power(name)
+VALUES ('Pretend');
+
+INSERT INTO public.power(name)
+VALUES ('Ossify');
--- src/docker-compose/spring-backend/src/main/resources/schema.sql
+++ src/docker-compose/spring-backend/src/main/resources/schema.sql
@@ -1,4 +1,30 @@
-CREATE TABLE IF NOT EXISTS GREETINGS (
-    id serial PRIMARY KEY,
-    name varchar(50) NOT NULL
+CREATE TABLE IF NOT EXISTS public.player (
+    id SERIAL NOT NULL,
+    name TEXT NOT NULL,
+    flavour_text TEXT,
+    wins INTEGER DEFAULT 0 NOT NULL,
+    losses INTEGER DEFAULT 0 NOT NULL,
+    draws INTEGER DEFAULT 0 NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS public.power (
+    id SERIAL NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT,
+    flavour_text TEXT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS public.power_ranking (
+    player_id INTEGER NOT NULL,
+    power_id INTEGER NOT NULL,
+    ranking INTEGER NOT NULL,
+    power_uses INTEGER DEFAULT 0 NOT NULL,
+    CONSTRAINT fk_player
+          FOREIGN KEY(player_id)
+            REFERENCES public.player(id),
+    CONSTRAINT fk_power
+              FOREIGN KEY(power_id)
+                REFERENCES public.power(id)
 );
